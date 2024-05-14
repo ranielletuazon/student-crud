af0dd089641085d3246dcd3f5393850f7917d237
ranielletuazon <radtuazon.student@ua.edu.ph>
2024-05-14 21:22:01 +0800

    first commit

---
+++ car/src/main/java/com/raintuazon/student/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.raintuazon.student;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, Integer>{
+}
---
+++ car/src/main/java/com/tuazon/student/Student.java
@@ -0,0 +1,57 @@
+package com.tuazon.student;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    private String firstName;
+    private String lastName;
+    private String course;
+    private String studentNumber;
+}
